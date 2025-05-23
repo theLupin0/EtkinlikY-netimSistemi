7c290e4138f8b26395a5a6ab53dd534bf09a7ad0
theLupin0 <162138514+theLupin0@users.noreply.github.com>
2025-05-23 10:37:52 +0300

    Update login.php

--- login.php
+++ login.php
@@ -19,29 +19,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
         if (password_verify($password, $user['password'])) {
-            
+
             if ($user['is_verified'] == 0) {
                 $error = "Hesabınız henüz admin tarafından onaylanmamış.";
-            }else {
-            // Giriş başarılı
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['username'] = $user['username'];
-            $_SESSION['role'] = $user['role'];
+            } else {
+                // Giriş başarılı
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['role'] = $user['role'];
 
-            if($user['first_login'] == 1){
-                header("Location: refreshPassword.php");
-            }else{
-                $sql1 = "UPDATE users SET is_active = 1 WHERE email='$email'";
-                $conn->query($sql1);
-                header("Location: anaEkran.php");
-            exit;
+                if ($user['first_login'] == 1) {
+                    header("Location: refreshPassword.php");
+                } else {
+                    $sql1 = "UPDATE users SET is_active = 1 WHERE email='$email'";
+                    $conn->query($sql1);
+                    header("Location: anaEkran.php");
+                    exit;
+                }
             }
-        }
         } else {
             $error = "Giriş Bilgileri Hatalı";
         }
     } else {
-        $error ="Kullanıcı bulunamadı";
+        $error = "Kullanıcı bulunamadı";
     }
     $conn->close();
 }
@@ -49,27 +49,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!DOCTYPE html>
 <html lang="tr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giriş Yap</title>
     <link rel="stylesheet" href="css.file/styleLogin.css">
 </head>
+
 <body>
     <h2>Giriş Yap</h2>
     <form method="POST" action="">
         <label class="mail" for="email">E-posta:</label>
         <input type="email" id="email" name="email" required><br><br>
-        
-        <label class="pas"for="password">Şifre:</label>
+
+        <label class="pas" for="password">Şifre:</label>
         <input type="password" id="password" name="password" required><br><br>
 
         <button type="submit">Giriş Yap</button><br><br><br>
         <p>Hesabınız yok mu? <a href="register.php">Kayıt Olun</a></p>
         <br>
         <?php if (!empty($error)) : ?>
-        <center><label id="errorMessage"><?php echo $error?></label></center>
+            <center><label id="errorMessage"><?php echo $error ?></label></center>
         <?php endif; ?>
     </form>
 </body>
+
 </html>
